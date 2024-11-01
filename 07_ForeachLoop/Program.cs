4b7e9d5163d7ebb8aca9fd7b563d86e3821af423
Hüseyin TEMEL <47536922+htemel@users.noreply.github.com>
2024-11-01 15:28:53 +0300

    Ders 7 Sonu

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+            //Foreach(1;2;3;4)
+
+            //1: Değişken Türü
+            //2: Değişken Adı
+            //3: In
+            //4: Liste, Koleksiyon, Dizi
+
+            //string[] cities = { "milano", "roma", "budapeşte", "ankara", "istanbul", "varşova" };
+            //foreach(string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //foreach(int x in numbers)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //foreach(int x in numbers)
+            //{
+            //    if(x%2==0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //int total = 0;
+            //foreach (int x in numbers)
+            //{
+            //    total += x;
+            //}
+            //Console.WriteLine(total);
+
+            //List<int> numbers=new List<int>()
+            //{
+            //    1,2,3,4,5,6,7,8,9
+            //};
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //string word = "Merhaba";
+            //foreach (char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            #endregion
+
+            #region Örnek Sınav Sistemi Uygulaması
+            Console.Write("***** C# Eğitim Kampı Sınav Uygulaması *****");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //Sınıftaki Öğrenci Sayısını Kullanıcıdan Alma
+            Console.WriteLine("----------------------------------");
+            Console.Write("Sınıfınızda Kaç Öğrenci Var: ");
+            int studentCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+
+            string[] studentNames=new string[studentCount];
+            double[] studentExamAvg=new double[studentCount];
+
+            for(int i=0; i<studentCount; i++)
+            {
+                Console.Write($"{i + 1}. öğrencinin ismini giriniz: ");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExamResult = 0;
+                //Her öğrenci için 3 sınav notu girişi
+                for(int j=0;j<3; j++)
+                {
+                    Console.Write($"{studentNames[i]} isimli öğrencinin {j + 1}. sınav notunu giriniz: ");
+                    double value=double.Parse(Console.ReadLine());
+                    totalExamResult += value;//notları topluyoruz.
+                }
+                Console.WriteLine();
+                studentExamAvg[i] = totalExamResult / 3;
+            }
+
+            //Sınav ortalamaları
+            for(int i=0;i<studentCount;i++)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {studentExamAvg[i]}");
+                if(studentExamAvg[i] >= 50)
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersi geçti");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı.");
+                    Console.WriteLine();
+                }
+                Console.WriteLine("----------------------------");
+            }
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
